b2ed66fa0962fb4741cc6d1bca5e32408f7fb1dd
ErikPervious <erickmatheus3000@gmail.com>
2022-05-02 18:35:59 -0300

    background color fixed

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { FirebaseProvider } from './src/contexts/useFirebase';
 
 import { Routes } from './src/routes';
 
+import { colors } from './src/styles';
+
 export default function App() {
-  return (  
-    <NavigationContainer>
-      <FirebaseProvider>
-        <Routes />
-      </FirebaseProvider>
-    </NavigationContainer>
+  return (
+    <View style={{backgroundColor: colors.BLACK_TERTIARY, flex: 1}}>
+      <NavigationContainer>
+        <FirebaseProvider>
+          <Routes />
+        </FirebaseProvider>
+      </NavigationContainer>
+    </View>
   );
 }
